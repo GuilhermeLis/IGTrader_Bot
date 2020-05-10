081f5ff4acfebc67315c0ab298abc70d9c735b7e
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-09 22:17:02 -0300

    catch erros

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import {TelegramBot} from 'node-telegram-bot-api'
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = "1246572158:AAFLv-CBbERI031otzQzM6tuWK-nsmvewyQ";
@@ -11,8 +12,12 @@ function newMensagem (response){
   const {id} = response.chat;
   const { first_name, username } = response.new_chat_participant;
   mensagem =
-    `Olá, ${first_name} ( @${username} ) seja bem-vindo ao IG do Trader FREE 🚀💰. Todos podem se expressar neste grupo, porém qualquer forma de DESRESPEITO, PORNOGRAFIA, RACISMO ou VENDEDORES DE QUALQUER COISA serão expulsos!!!`;
+    `Olá, ${first_name} ( @${username} ) seja bem-vindo ao IG do Trader FREE 🚀💰.` + 
+    `Todos podem se expressar neste grupo, porém qualquer forma de DESRESPEITO,`
+    + ` PORNOGRAFIA, RACISMO ou VENDEDORES DE QUALQUER COISA serão expulsos!!!`;
   bot.sendMessage(id, mensagem);
 }
 
+try {
 bot.on('new_chat_members', newMensagem)
+}catch(e){console.log(e)}

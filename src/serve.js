20e07f37b5f49c448fd11697615a7461270edd4f
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-10 20:50:47 -0300

    new serve port

--- src/serve.js
+++ src/serve.js
@@ -3,7 +3,7 @@ import express from "express";
 import './index.js'
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 
 app.get('/',(require, response)=> response.send('Hello world'))

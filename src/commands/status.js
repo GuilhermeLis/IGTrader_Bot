9573e7d88ad4c333a758271f46b012a5970ed010
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-10 20:12:32 -0300

    trying to serve

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,7 +1,6 @@
 import { bot } from '~/config/bot'
 
 export default function start(response) {
-  // console.log(mensagem)
   const { from } = response;
   const reply = `I''m still on @${from.username}`
   bot.sendMessage(from.id,reply)

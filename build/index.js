f82d06f4a42f1f98e5adcc7fa4b3e43cf8b988f7
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-10 01:25:45 -0300

    new configurations

---
+++ build/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var _nodeTelegramBotApi = _interopRequireDefault(require("node-telegram-bot-api"));
+
+require("dotenv/config");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var token;
+
+if (process.env.DESKTOP_SESSION === "deepin") {
+  token = process.env.DEV_TOKEN;
+} else {
+  token = process.env.BUILD_TOKEN;
+}
+
+var bot = new _nodeTelegramBotApi["default"](token, {
+  polling: true
+});
+bot.onText(/\/echo (.+)/, function (msg, match) {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  var chatId = msg.chat.id;
+  var resp = match[1]; // the captured "whatever"
+  // send back the matched "whatever" to the chat
+
+  bot.sendMessage(chatId, resp);
+});
+
+function newMensagem(response) {
+  var id = response.chat.id;
+  var _response$new_chat_pa = response.new_chat_participant,
+      first_name = _response$new_chat_pa.first_name,
+      username = _response$new_chat_pa.username;
+  var mensagem = "Ol\xE1, ".concat(first_name, " ( @").concat(username, " ) seja bem-vindo ao IG do Trader FREE \uD83D\uDE80\uD83D\uDCB0.") + "Todos podem se expressar neste grupo, por\xE9m qualquer forma de DESRESPEITO," + " PORNOGRAFIA, RACISMO ou VENDEDORES DE QUALQUER COISA ser\xE3o expulsos!!!";
+  bot.sendMessage(id, mensagem);
+}
+
+try {
+  bot.on("new_chat_members", newMensagem);
+} catch (e) {
+  console.log(e);
+}
+//# sourceMappingURL=index.js.map
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// import {TelegramBot} from 'node-telegram-bot-api'
-const TelegramBot = require('node-telegram-bot-api');
-require("dotenv").config();
+import TelegramBot from "node-telegram-bot-api";
+import 'dotenv/config'
 
 let token;
-
-
 
 if (process.env.DESKTOP_SESSION === "deepin") {
   token = process.env.DEV_TOKEN;
@@ -12,23 +9,35 @@ if (process.env.DESKTOP_SESSION === "deepin") {
   token = process.env.BUILD_TOKEN;
 }
 
-// console.log(token)
-
 const bot = new TelegramBot(token, {
-  polling: true
+  polling: true,
 });
 
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-function newMensagem (response){
-  const {id} = response.chat;
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+function newMensagem(response) {
+  const { id } = response.chat;
   const { first_name, username } = response.new_chat_participant;
-  mensagem =
-    `Olá, ${first_name} ( @${username} ) seja bem-vindo ao IG do Trader FREE 🚀💰.` + 
-    `Todos podem se expressar neste grupo, porém qualquer forma de DESRESPEITO,`
-    + ` PORNOGRAFIA, RACISMO ou VENDEDORES DE QUALQUER COISA serão expulsos!!!`;
+  const mensagem =
+    `Olá, ${first_name} ( @${username} ) seja bem-vindo ao IG do Trader FREE 🚀💰.` +
+    `Todos podem se expressar neste grupo, porém qualquer forma de DESRESPEITO,` +
+    ` PORNOGRAFIA, RACISMO ou VENDEDORES DE QUALQUER COISA serão expulsos!!!`;
   bot.sendMessage(id, mensagem);
 }
 
 try {
-bot.on('new_chat_members', newMensagem)
-}catch(e){console.log(e)}
+  bot.on("new_chat_members", newMensagem);
+} catch (e) {
+  console.log(e);
+}
+

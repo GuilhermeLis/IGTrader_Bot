5d213a567e07b3498f418b4a64e0793d447d16d5
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-10 20:08:08 -0300

    ban command

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -3,21 +3,24 @@ import 'dotenv/config'
 
 export default async function ban(response) {
 
+  const { from } = response;
   const { reply_to_message } = response;
-  const { forward_from } = reply_to_message;
   const { chat } = response;
-  // const { id } = response.chat;
-  // const { from } = forward_from;
 
+  if(!reply_to_message){
+     const reply = ` ${from.first_name} ( @${from.username} ) O comando` +
+       ` /ban deve ser usado em resposta a uma mensagem`
+     bot.sendMessage(chat.id, reply)
+     return;
+  }
+
+  const { forward_from } = reply_to_message;
   const chat_id = process.env.CHAT_ID;
 
   const administrator = process.env.ADMINISTRATOR;
 
-  // const { status } = await bot.getChatMember(chat.id, idc)
-  // console.log(response)
-  console.log(chat_id, forward_from.id)
-  const { member } = await bot.getChatMember(chat_id, forward_from.id)
-  console.log(member)
+  const member = await bot.getChatMember(chat_id, forward_from.id)
+
   if (chat.id != administrator) {
 
     const reply = "Você não tem permissão para usar essa função"
@@ -31,6 +34,14 @@ export default async function ban(response) {
     bot.sendMessage(administrator,reply)
     return;
   }else{
-    bot.kickChatMember(chat_id, forward_from.id)
+    const reply = `Usuário ( @${forward_from.username} ) foi retirado do grupo`
+    const justify = `@${forward_from.username} foi retirado por`
+                    +` não cumprir com as politicas do IGTrader`
+    bot.kickChatMember(chat_id, forward_from.id).then(()=>{
+      bot.sendMessage(administrator,reply)
+      bot.sendMessage(chat_id,justify)
+    }).catch(()=>
+      bot.sendMessage(administrator, "não foi possível efetuar a operação")
+    )
   }
 }

326f9a9185990683a8aba7c53503d0af5e74fa06
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-10 02:01:25 -0300

    new script build

--- build/index.js
+++ build/index.js
@@ -18,12 +18,15 @@ var bot = new _nodeTelegramBotApi["default"](token, {
   polling: true
 });
 bot.onText(/\/echo (.+)/, function (msg, match) {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
   var chatId = msg.chat.id;
   var resp = match[1]; // the captured "whatever"
   // send back the matched "whatever" to the chat
+
+  bot.sendMessage(chatId, resp);
+});
+bot.onText(/\/start/, function (msg, match) {
+  var chatId = msg.chat.id;
+  var resp = "Você está ativando o bot de gerencia do IGTrader 🚀💰"; // send back the matched "whatever" to the chat
 
   bot.sendMessage(chatId, resp);
 });
--- src/index.js
+++ src/index.js
@@ -14,12 +14,20 @@ const bot = new TelegramBot(token, {
 });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+
 
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/start/, (msg, match) => {
+
+
+  const chatId = msg.chat.id;
+  const resp = "Você está ativando o bot de gerencia do IGTrader 🚀💰"
 
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);

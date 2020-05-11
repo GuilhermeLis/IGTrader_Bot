41e0c61f20bf673271832a84489cdd8ab163b903
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-10 22:24:26 -0300

    new run script

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,7 @@
 import { bot } from '~/config/bot'
 
 export default async function help(mensagem) {
+  console.log(mensagem)
   const { chat } = mensagem;
 
   const reply = "Esté é um bot que tem como objetivo ajudar na administração" +

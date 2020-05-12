c3dfa99a079236b7815bf82093abba2c07280abc
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-12 17:41:08 -0300

    new wellcome mensagem

--- src/actions/newUser.js
+++ src/actions/newUser.js
@@ -2,10 +2,10 @@ import { bot } from '~/config/bot'
 
 export default function newUser(response) {
   const { id } = response.chat;
-  const { first_name, username } = response.new_chat_participant;
+  const { first_name } = response.new_chat_participant;
 
   const mensagem =
-    `Olá, ${first_name} ( @${username} ) seja bem-vindo ao IG do Trader FREE 🚀💰.` +
+    `Olá, ${first_name} seja bem-vindo ao IG do Trader FREE 🚀💰.` +
     `Todos podem se expressar neste grupo, porém qualquer forma de DESRESPEITO,` +
     ` PORNOGRAFIA, RACISMO ou VENDEDORES DE QUALQUER COISA serão expulsos!!!`;
 
